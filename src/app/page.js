ebba23efb44e92bded39833d6cc2b771293cff5b
Netbul4 <eddiepp06@gmail.com>
2024-01-25 15:53:05 -0400

    feat: API crud and helpers

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import "../components/index.css";
 import CrudApp from "@/components/CrudApp";
+import CrudApi from "@/components/CrudApi";
 
 export default function Home() {
   return (
     <>
       <h1>React Tasks</h1>
+      <hr/>
+      <CrudApi/>
+      <hr/>
       <CrudApp/>
     </>
   );
---
+++ src/components/CrudApi.js
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState } from "react";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+
+const CrudApi = () => {
+  const [db, setDb] = useState([]);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  const createData = (data) => {
+    data.id = Date.now();
+    setDb([...db, data]);
+  };
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `Are you sure to delete this register with the id '${id}?`
+    );
+
+    if (!isDelete) {
+      return;
+    } else {
+      let newData = db.filter((el) => el.id != id);
+      setDb(newData);
+    }
+  };
+
+  return (
+    <div>
+      <h2>CRUD Api</h2>
+      <article classname="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        <CrudTable
+          data={db}
+          setDataToEdit={setDataToEdit}
+          deleteData={deleteData}
+        />
+      </article>
+    </div>
+  );
+};
+
+export default CrudApi;
